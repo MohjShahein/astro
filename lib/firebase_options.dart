dadebf19626c937d80a18f0d51c6f5868eb8ea6a
Mohammad Shahein <mohj.shahein@gmail.com>
2025-05-02 15:59:28 +0200

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -32,29 +32,14 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyAHprGUvVMn_KPWsTuI_YRULihOiUecS5w',
-    appId: '1:776961382756:android:74f9698db63760e1958271',
-    messagingSenderId: '776961382756',
-    projectId: 'astrology-d317e',
-    storageBucket: 'astrology-d317e.appspot.com',
+
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyAHprGUvVMn_KPWsTuI_YRULihOiUecS5w',
-    appId: '1:776961382756:ios:74f9698db63760e1958271',
-    messagingSenderId: '776961382756',
-    projectId: 'astrology-d317e',
-    storageBucket: 'astrology-d317e.appspot.com',
-    iosClientId: '776961382756-app-version14sa.apps.googleusercontent.com',
+
   );
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyCIsFZWb-FF_9y3mOZSBwI1TB5er3rXNUU',
-    appId: '1:776961382756:web:f8bd64f9dfc26cd7958271',
-    messagingSenderId: '776961382756',
-    projectId: 'astrology-d317e',
-    authDomain: 'astrology-d317e.firebaseapp.com',
-    storageBucket: 'astrology-d317e.appspot.com',
-    measurementId: 'G-LWN0DNF85G',
+
   );
 }
